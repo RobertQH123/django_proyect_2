c394ffc2745dca90b94548deaff3e0ea4093f28e
RobertQH123 <robertqhjaki@gmail.com>
2024-02-16 07:15:58 -0500

    crear registros en la base de datos, listarlos en una platilla de la app

--- universidad/Academico/views.py
+++ universidad/Academico/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from Academico.models import Curso
 
 # Create your views here.
+
+
+def home(request):
+    cur = Curso.objects.all()
+    return render(request, "cursoAllTemplate.html",{"cursos":cur})
